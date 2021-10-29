37651e58af68c5abb81c58f06be40c3b31ea5587
Willy Ricardo Wijaya <willyric17@gmail.com>
2021-10-29 11:44:04 +0700

    update refresh state script

--- script/updateplantstate.sh
+++ script/updateplantstate.sh
@@ -1,6 +1,16 @@
 lastmaster=$(basename "$(ls plant/master | sort | tail -n1)" .json)
+updates=$(ls plant/update | sort)
 
-output="{\"master\":$lastmaster,\"updates\":["
-output="$output]}"
+first=1
+for f in $updates; do
+    if [ $first -eq 1 ]; then
+        new_update="$(basename "$f" .json)"
+        first=0
+    else
+        new_update="$new_update,$(basename "$f" .json)"
+    fi
+done
+
+output="{\"master\":$lastmaster,\"updates\":[$new_update]}"
 
 echo $output > plant/state.json
